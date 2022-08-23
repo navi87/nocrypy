38f24eed99994bcdf1f97b6e1a560a5a3cf470f5
navi87 <36727890+navi87@users.noreply.github.com>
2022-08-23 22:55:33 +0200

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,21 @@
+import os
+from cryptography.fernet import Fernet
+
+files = []
+
+for file in os.listdir():
+	if file == "crypt.py" or file == "thekey.key" or file == "decrypt.py":
+		continue
+	files.append(file)
+
+print(files)
+
+with open("thekey.key", "rb") as key:
+	secretkey = key.read()
+
+for file in files:
+	with open(file, "rb") as thefile:
+		contents = thefile.read()
+	contents_decrypted = Fernet(secretkey).decrypt(contents)
+	with open(file, "wb") as thefile:
+		thefile.write(contents_decrypted)
